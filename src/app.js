2e5f39cec58c90db51a86bcab2dd9408f495c171
Luis Cipriani <cunhaluisg5@gmail.com>
2020-08-18 21:56:58 -0300

    Adição de token de usuário

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const nfceRouter = require('./routers/nfce')
 const userRouter = require('./routers/user')
+const authRouter = require('./routers/project');
 const port = process.env.PORT
 require('./db/db')
 
@@ -9,6 +10,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(authRouter)
 app.use(nfceRouter)
 app.use(userRouter)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth');
 
 const router = express.Router();
+
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    });
+}
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -15,10 +25,32 @@ router.post('/register', async (req, res) => {
         await user.save();
         user.password = undefined;
 
-        return res.status(201).send({ user });
+        return res.status(201).send({ user, token: generateToken({ id: user.id }) });
     } catch (err) {
         return res.status(400).send({ error: 'Registration failed' });
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if(!user) {
+        return res.status(400).send({ error: 'User not found' });
+    }
+
+    if(!await  bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ error: 'Invalid password' });
+    }
+
+    user.password = undefined;
+
+    const token = jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: 86400
+    });
+
+    res.send({ user, token: generateToken({ id: user.id }) });
 })
 
 module.exports = router
