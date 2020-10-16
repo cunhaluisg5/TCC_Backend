9e8406327c35f49be764ef9e4c8a1a9affabbe63
Luis Cipriani <cunhaluisg5@gmail.com>
2020-10-16 20:07:58 -0300

    Adição do cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const port = process.env.PORT;
 require('./db/db');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 require('./app/controllers/index')(app);
